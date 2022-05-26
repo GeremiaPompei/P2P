2255bcef322273a0d6043fc5505a56e9eab60807
Geremia Pompei <geremiapompei@gmail.com>
2022-05-26 18:52:54 +0200

    implemented try

--- finalterm/TRY/contracts/ERC721.sol
+++ finalterm/TRY/contracts/ERC721.sol
@@ -10,7 +10,9 @@ contract ERC721 is IERC721, IERC721Metadata {
     string private __name;
     string private __symbol;
     string private __baseURI;
+    uint256 private __counter = 0;
 
+    mapping(uint256 => string) private __tokens;
     mapping(uint256 => address) private __owners;
     mapping(address => uint256) private __balances;
     mapping(uint256 => address) private __tokenApprovals;
@@ -22,22 +24,99 @@ contract ERC721 is IERC721, IERC721Metadata {
         __baseURI = _baseURI;
     }
 
-    function balanceOf(address _owner) override external view returns (uint256) {
+    modifier __isApprovedOrOwner(address _from, uint256 _tokenId) {
+        require(_tokenId != 0, "Token does not exists");
+        address owner = this.ownerOf(_tokenId);
+        require(_from == owner, "Owner does not own the token");
+        address spender = msg.sender;
+        require(spender == owner || __operatorApprovals[owner][spender] || this.getApproved(_tokenId) == spender, "Spender unauthorized");
+        _;
+    }
+
+    function balanceOf(address _owner) override virtual external view returns (uint256) {
         require(_owner != address(0), "Invalid owner address");
         return __balances[_owner];
     }
 
-    function ownerOf(uint256 _tokenId) override external view returns (address) {
+    function ownerOf(uint256 _tokenId) override virtual external view returns (address) {
         address owner = __owners[_tokenId];
         require(owner != address(0), "Token does not exists");
         return owner;
     }
 
-    modifier __isApprovedOrOwner(address spender, uint256 tokenId) {
-        require(tokenId != 0, "Token does not exists");
-        address owner = this.ownerOf(tokenId);
-        require(spender == owner || __operatorApprovals[owner][spender] || this.getApproved(tokenId) == spender, "Spender unauthorized");
-        _;
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes memory _data) override virtual external {
+        __transferFrom(_from, _to, _tokenId);
+        __checkContract(_from, _to, _tokenId, _data);
+    }
+
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) override virtual external {
+        this.safeTransferFrom(_from, _to, _tokenId);
+
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) override virtual external {
+        __transferFrom(_from, _to, _tokenId);
+    }
+
+    function approve(address _approved, uint256 _tokenId) override virtual external {
+        __tokenApprovals[_tokenId] = _approved;
+        emit Approval(msg.sender, _approved, _tokenId);
+    }
+
+    function setApprovalForAll(address _operator, bool _approved) override virtual external {
+        __operatorApprovals[msg.sender][_operator] = _approved;
+        emit ApprovalForAll(msg.sender, _operator, _approved);
+    }
+
+    function getApproved(uint256 _tokenId) override virtual external view returns (address) {
+        return __tokenApprovals[_tokenId];
+    }
+
+    function isApprovedForAll(address _owner, address _operator) override virtual external view returns (bool) {
+       return __operatorApprovals[_owner][_operator];
+    }
+
+    function name() override virtual external view returns (string memory _name) {
+        return __name;
+    }
+    
+    function symbol() override virtual external view returns (string memory _symbol) {
+        return __symbol;
+    }
+    
+    function tokenURI(uint256 _tokenId) override virtual external view returns (string memory) {
+        require(__owners[_tokenId] != address(0), "Token does not exists");
+        string memory baseURI = __baseURI;
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, __tokens[_tokenId])) : "";
+    }
+
+    function safeMint(address _to, string memory _uri, bytes memory _data) external returns(uint256 _tokenId) {
+        _tokenId = __mint(_to, _uri);
+        __checkContract(address(0), _to, _tokenId, _data);
+    }
+
+    function safeMint(address _to, string memory _uri) external returns(uint256 _tokenId) {
+        _tokenId = this.safeMint(_to, _uri);
+    }
+
+    function mint(address _to, string memory _uri) external returns(uint256 _tokenId) {
+        _tokenId = __mint(_to, _uri);
+    }
+
+    function __checkContract(address _from, address _to, uint256 _tokenId, bytes memory _data) private {
+        if(__isContract(_to)) {
+            try IERC721TokenReceiver(_to).onERC721Received(msg.sender, _from, _tokenId, _data) returns (bytes4 retval) {
+                require(retval == IERC721TokenReceiver.onERC721Received.selector);
+            } catch (bytes memory reason) {
+                assembly { revert(add(32, reason), mload(reason)) }
+            }
+        }
+    }
+
+    function __isContract(address addr) private view returns (bool) {
+        uint size;
+        assembly { size := extcodesize(addr) }
+        return size > 0;
     }
 
     function __transferFrom(address _from, address _to, uint256 _tokenId) private __isApprovedOrOwner(_from, _tokenId) {
@@ -47,81 +126,12 @@ contract ERC721 is IERC721, IERC721Metadata {
         emit Transfer(_from, _to, _tokenId);
     }
 
-    function __checkContract(address _from, address _to, uint256 _tokenId, bytes memory _data) private {
-        if(msg.sender != tx.origin) {
-            try IERC721TokenReceiver(_to).onERC721Received(msg.sender, _from, _tokenId, _data) returns (bytes4 retval) {
-                require(retval == IERC721TokenReceiver.onERC721Received.selector);
-            } catch (bytes memory reason) {
-                assembly { revert(add(32, reason), mload(reason)) }
-            }
-        }
-    }
-
-    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes memory _data) override external {
-        __transferFrom(_from, _to, _tokenId);
-        __checkContract(_from, _to, _tokenId, _data);
-    }
-
-    function safeTransferFrom(address _from, address _to, uint256 _tokenId) override external {
-        this.safeTransferFrom(_from, _to, _tokenId);
-
-    }
-
-    function transferFrom(address _from, address _to, uint256 _tokenId) override external {
-        __transferFrom(_from, _to, _tokenId);
-
-    }
-
-    function approve(address _approved, uint256 _tokenId) override external {
-        __tokenApprovals[_tokenId] = _approved;
-        emit Approval(msg.sender, _approved, _tokenId);
-    }
-
-    function setApprovalForAll(address _operator, bool _approved) override external {
-        __operatorApprovals[msg.sender][_operator] = _approved;
-        emit ApprovalForAll(msg.sender, _operator, _approved);
-    }
-
-    function getApproved(uint256 _tokenId) override external view returns (address) {
-        return __tokenApprovals[_tokenId];
-    }
-
-    function isApprovedForAll(address _owner, address _operator) override external view returns (bool) {
-       return __operatorApprovals[_owner][_operator];
-    }
-
-    function name() override external view returns (string memory _name) {
-        return __name;
-    }
-    
-    function symbol() override external view returns (string memory _symbol) {
-        return __symbol;
-    }
-    
-    function tokenURI(uint256 _tokenId) override external view returns (string memory) {
-        require(__owners[_tokenId] != address(0), "Token does not exists");
-        string memory baseURI = __baseURI;
-        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, __uintToString(_tokenId))) : "";
-    }
-
-    function __uintToString(uint _i) internal pure returns (string memory _uintAsString) {
-        if (_i == 0)
-            return "0";
-        uint j = _i;
-        uint len;
-        while (j != 0) {
-            len++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(len);
-        uint k = len;
-        while (_i != 0) {
-            k = k-1;
-            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
-            bytes1 b1 = bytes1(temp);
-            bstr[k] = b1;
-            _i /= 10;
-        }
-        return string(bstr);
+    function __mint(address _to, string memory _uri) private returns(uint256 _tokenId) {
+        __counter += 1;
+        _tokenId = __counter;
+        __tokens[_tokenId] = _uri;
+        __owners[_tokenId] = _to;
+        __balances[_to] += 1;
+        emit Transfer(address(0), _to, _tokenId);
     }
 }
--- finalterm/TRY/contracts/TRY.sol
+++ finalterm/TRY/contracts/TRY.sol
@@ -4,5 +4,127 @@ pragma solidity ^0.8.0;
 import './ERC721.sol';
 
 contract TRY {
-    
+
+    enum Classes {None, Class_1, Class_2, Class_3, Class_4, Class_5, Class_6, Class_7, Class_8}
+
+    struct Collectible {
+        uint256 tokenId;
+        string uri;
+        bool available;
+    }
+
+    uint8 private constant TOTAL_NUMBERS = 6;
+    uint8 private constant POWERBALL_POSITION = 5;
+    uint8 private constant DURATION = 10;
+    uint8[2][TOTAL_NUMBERS] private RANGES = [[1, 69], [1, 69], [1, 69], [1, 69], [1, 69], [1, 26]];
+
+    address private __erc721Address;
+    bool private __roundActive;
+    uint256 private __startRoundBlockNumber;
+    address private __operator;
+    uint16 private __k;
+    uint256 private __ticketPrice;
+
+    uint8[TOTAL_NUMBERS] private __winningNumbers;
+    address[] private __players;
+    Collectible[] private __collectibles;
+    uint8[TOTAL_NUMBERS][] private __tickets;
+
+    constructor(address _erc721Address, uint16 _k, uint256 _ticketPrice) {
+        __operator = msg.sender;
+        __erc721Address = _erc721Address;
+        __k = _k + 1 % 256;
+        __roundActive = true;
+        __ticketPrice = _ticketPrice;
+    }
+
+    modifier __isOperator() {
+        require(__operator == msg.sender, "Operation unauthorized");
+        _;
+    }
+
+    modifier __isRoundActive() {
+        require(__roundActive && block.number - __startRoundBlockNumber < DURATION, "Round is not active");
+        _;
+    }
+
+    function startNewRound() external {
+        require(!__roundActive, "Round is not finished");
+        __roundActive = true;
+        __startRoundBlockNumber = block.number;
+    }
+
+    function buy(uint8[TOTAL_NUMBERS] memory numbers) external payable __isRoundActive() {
+        require(msg.value >= __ticketPrice, "Not enough ether");
+        __players.push(msg.sender);
+        __tickets.push(numbers);
+        payable(msg.sender).transfer(msg.value - __ticketPrice);
+    }
+
+    function drawNumbers() external __isOperator() {
+        uint256 number = __random();
+        for(uint8 i = 0; i < TOTAL_NUMBERS; i++) {
+            __winningNumbers[i] = uint8(number) % RANGES[i][1] + RANGES[i][0];
+            number /= 100;
+        }
+    }
+
+    function givePrizes() external __isOperator() {
+        for(uint8 i = 0; i < __players.length; i++) {
+            if(__matchClass(__tickets[i]) != Classes.None) {
+                Collectible storage _collectibles = __collectibles[i];
+                require(_collectibles.tokenId > 0, "Not enough prizes");
+                if(_collectibles.available){
+                    ERC721(__erc721Address).safeTransferFrom(address(this), __players[i], _collectibles.tokenId);
+                    _collectibles.available = false;
+                } else {
+                    ERC721(__erc721Address).mint(__players[i], _collectibles.uri);
+                }
+            }
+        }
+        payable(__operator).transfer(address(this).balance);
+        this.startNewRound();
+    }
+
+    function mint(string memory _uri) external __isOperator() {
+        uint256 _tokenId = ERC721(__erc721Address).mint(address(this), _uri);
+        if(__collectibles.length + 1 > uint8(Classes.Class_8))
+            __collectibles.pop();
+        __collectibles.push(Collectible(_tokenId, _uri, true));
+    }
+
+    function closeLottery() external __isOperator() {
+        require(__roundActive, "Lottery already closed");
+        __roundActive = false;
+    }
+
+    function __random() private view returns(uint256) {
+        return uint256(keccak256(abi.encode(blockhash(block.number - __k))));
+    }
+
+    function __matchClass(uint8[TOTAL_NUMBERS] memory _ticket) private view returns(Classes class) {
+        uint8 stdMatches = 0;
+        for(uint8 i = 0; i < TOTAL_NUMBERS - 1; i++)
+            if(_ticket[i] == __winningNumbers[i])
+                stdMatches += 1;
+        bool matchPowerball = _ticket[POWERBALL_POSITION] == __winningNumbers[POWERBALL_POSITION];
+        if(stdMatches == 5 && matchPowerball)
+            return Classes.Class_1;
+        if(stdMatches == 5)
+            return Classes.Class_2;
+        if(stdMatches == 4 && matchPowerball)
+            return Classes.Class_3;
+        if(stdMatches == 4 || stdMatches == 3 && matchPowerball)
+            return Classes.Class_4;
+        if(stdMatches == 3 || stdMatches == 2 && matchPowerball)
+            return Classes.Class_5;
+        if(stdMatches == 2 || stdMatches == 1 && matchPowerball)
+            return Classes.Class_6;
+        if(stdMatches == 1)
+            return Classes.Class_7;
+        if(matchPowerball)
+            return Classes.Class_8;
+        return Classes.None;
+    }
+
 }
